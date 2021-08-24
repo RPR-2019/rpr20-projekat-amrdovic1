69f0c375cd9c889c1d553a001cc1673aa188147f
amrdovic <72739061+amrdovic1@users.noreply.github.com>
2021-08-24 13:43:15 +0200

    More FXML files for reservation added

--- src/RPRMovieApp/controllers/ReservationController.java
+++ src/RPRMovieApp/controllers/ReservationController.java
@@ -1,4 +1,23 @@
 package RPRMovieApp.controllers;
 
-public class ReservationController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class ReservationController
+{
+    public Label movieName;
+    public Label screeningTime;
+    public Label noOfTickets;
+    public Label price;
+    public Button seatsBtn;
+    public Button cancelBtn;
+    public Button finishBtn;
+    public Button foodBtn;
+
+    @FXML
+    public void initialize()
+    {
+
+    }
 }
